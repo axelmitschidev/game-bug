3ba93ecfcc73d4f0cc005d77ad1eabaac9f17b11
Axel Mitschi <axel.mitschi@gmail.com>
2021-10-09 00:28:45 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './assets/css/style.css'
+import main from './assets/js/main'
+
+main()
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, './src/index.html') })]
+}
